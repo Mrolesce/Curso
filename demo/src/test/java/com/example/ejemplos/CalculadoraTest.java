cac8980ae3b02966b9b926c17824edfab0e6537c
mrolesce <mrolesce@host.docker.internal>
2023-03-24 10:07:33 +0100

    mockito

--- demo/src/test/java/com/example/ejemplos/CalculadoraTest.java
+++ demo/src/test/java/com/example/ejemplos/CalculadoraTest.java
@@ -1,7 +1,8 @@
 package com.example.ejemplos;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
@@ -99,6 +100,13 @@ class CalculadoraTest {
 			@CsvSource(value = {"1,1,2", "0.1, 0.2, 0.3", "3,4,7"}) //valores de cada vuelta
 			void testSumasOK(double op1, double op2, double rslt) {
 				assertEquals(rslt, calc.suma(op1, op2));
+			}
+			
+			@Test
+			void testSumaMock() {
+				Calculadora calc = mock(Calculadora.class);
+				when(calc.suma(2, 2)).thenReturn(3.0);
+				assertEquals(3, calc.suma(2, 2));
 			}
 
 		}
--- demo/src/test/java/com/example/ejemplos/PersonaTest.java
+++ demo/src/test/java/com/example/ejemplos/PersonaTest.java
@@ -1,15 +1,24 @@
 package com.example.ejemplos;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.RepetitionInfo;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.mockito.Mock;
 
 import com.example.core.test.Smoke;
+import com.example.exceptions.InvalidDataException;
+import com.example.ioc.PersonaRepository;
 
 class PersonaTest {
 
@@ -30,7 +39,7 @@ class PersonaTest {
 		assertAll("Validación objeto",
 				() -> assertEquals(1, p.getId(), "id"),
 				() -> assertEquals("Pepito", p.getNombre(), "getNombre"),
-				() -> assertEquals("Grillo", p.getApellidos(), "getApellidos"));
+				() -> assertEquals("Grillo", p.getApellidos().get(), "getApellidos"));
 		
 	}
 	
@@ -47,8 +56,42 @@ class PersonaTest {
 		assertAll("Validar propiedades",
 			() -> assertEquals(repetitionInfo.getCurrentRepetition(), p.getId(), "id"),
 			() -> assertEquals("Pepito" + repetitionInfo.getCurrentRepetition(), p.getNombre(), "getNombre"),
-			() -> assertEquals("Grillo", p.getApellidos(), "getApellidos")
+			() -> assertEquals("Grillo", p.getApellidos().get(), "getApellidos")
 		);
+	}
+	
+	@Nested
+	class PersonaRepositoryTest{
+		
+		PersonaRepository dao;
+		
+		@BeforeEach
+		void setUp() {
+			dao = mock(PersonaRepository.class);
+		}
+		
+		@Test
+		void testLoad() {
+			PersonaRepository dao = mock(PersonaRepository.class);
+			when(dao.load()).thenReturn(Persona.builder().id(1).nombre("Pepito").apellidos("Grillo").build());
+			
+			var p = dao.load();
+			
+			assertNotNull(p);
+			assertTrue(p instanceof Persona, "No es instancia de persona");
+			assertAll("Validación objeto",
+					() -> assertEquals(1, p.getId(), "id"),
+					() -> assertEquals("Pepito", p.getNombre(), "getNombre"),
+					() -> assertEquals("Grillo", p.getApellidos().get(), "getApellidos"));
+		}
+		
+		@Test
+		void testSave() throws InvalidDataException {
+			PersonaRepository dao = mock(PersonaRepository.class);
+			doThrow(new IllegalArgumentException()).when(dao).save(null);
+			
+			assertThrows(IllegalArgumentException.class, () -> dao.save(null));
+		}
 	}
 
 }
