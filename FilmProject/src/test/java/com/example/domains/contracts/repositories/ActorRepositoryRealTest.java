db456386e214f047484399ec66129c3041f674bf
mrolesce <mrolesce@LES-5CG0149KN3.corp.capgemini.com>
2023-03-31 09:39:22 +0200

    actorrepository test en base de datos real

---
+++ FilmProject/src/test/java/com/example/domains/contracts/repositories/ActorRepositoryRealTest.java
@@ -0,0 +1,24 @@
+package com.example.domains.contracts.repositories;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest()
+@ActiveProfiles("test")
+public class ActorRepositoryRealTest {
+
+	@Autowired
+	ActorRepository dao;
+	
+	@Test
+	void testFindAll() {
+		assertThat(dao.findAll().size()).isGreaterThanOrEqualTo(200);
+	}
+	
+	
+
+}
