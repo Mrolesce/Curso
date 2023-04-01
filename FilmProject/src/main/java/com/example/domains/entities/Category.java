78ea31c69648adbb88e6c38a5a3bf5e6baec7919
mrolesce <mrolesce@host.docker.internal>
2023-04-01 23:13:57 +0200

    new category constructor

--- FilmProject/src/main/java/com/example/domains/entities/Category.java
+++ FilmProject/src/main/java/com/example/domains/entities/Category.java
@@ -54,6 +54,12 @@ public class Category extends EntityBase<Category> implements Serializable {
 		this.categoryId = categoryId;
 	}
 
+	public Category(int categoryId, @NotBlank @Size(max = 25) String name) {
+		super();
+		this.categoryId = categoryId;
+		this.name = name;
+	}
+
 	public int getCategoryId() {
 		return this.categoryId;
 	}
