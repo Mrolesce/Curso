15dc1f329f81a45c6f48f5259099837b84d84e28
mrolesce <mrolesce@host.docker.internal>
2023-04-02 21:58:38 +0200

    more filmtests

--- FilmProject/src/test/java/com/example/domains/entities/FilmTest.java
+++ FilmProject/src/test/java/com/example/domains/entities/FilmTest.java
@@ -3,6 +3,8 @@ package com.example.domains.entities;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
@@ -23,6 +25,95 @@ class FilmTest {
 			var film = new Film("Hola mundo", new Language(2), (byte)1, new BigDecimal(10.0), 1, new BigDecimal(10.0));
 			
 			assertTrue(film.isValid());
+		}
+		@Test
+		void addActorTest() {
+			var film = new Film("Hola mundo", new Language(2), (byte)1, new BigDecimal(10.0), 1, new BigDecimal(10.0));
+			var actor1 = new Actor(0, "Marc", "Roles");
+			
+			film.addActor(actor1);
+			
+			assertTrue(film.getActors().contains(actor1));
+		}
+		@Test
+		void getActorsTest() {
+			var film = new Film("Hola mundo", new Language(2), (byte)1, new BigDecimal(10.0), 1, new BigDecimal(10.0));
+			var actor1 = new Actor(0, "Marc", "Roles");
+			var actor2 = new Actor(1, "Jose", "Antonio");
+			
+			film.addActor(actor1);
+			film.addActor(actor2);
+			
+			List<Actor> actors = new ArrayList<Actor>();
+			actors.add(actor1);
+			actors.add(actor2);
+			
+			assertEquals(actors, film.getActors());
+		}
+		@Test
+		void removeActorsTest() {
+			var film = new Film("Hola mundo", new Language(2), (byte)1, new BigDecimal(10.0), 1, new BigDecimal(10.0));
+			var actor1 = new Actor(0, "Marc", "Roles");
+			var actor2 = new Actor(1, "Jose", "Antonio");
+			
+			film.addActor(actor1);
+			film.addActor(actor2);
+	
+			film.removeActor(actor1);
+			film.removeActor(actor2);
+			
+			assertTrue(film.getActors().isEmpty());
+		}
+		@Test
+		void addCategoryTest() {
+			var film = new Film("Hola mundo", new Language(2), (byte)1, new BigDecimal(10.0), 1, new BigDecimal(10.0));
+			
+			var category = new Category(2);
+			film.addCategory(category);
+			
+			assertTrue(film.getCategories().contains(category));
+		}
+		@Test
+		void removeCategoriesTest() {
+			var film = new Film("Hola mundo", new Language(2), (byte)1, new BigDecimal(10.0), 1, new BigDecimal(10.0));
+			
+			var category = new Category(2);
+			var category2 = new Category(3);
+			film.addCategory(category);
+			film.addCategory(category2);
+			
+			film.removeCategory(category);
+			film.removeCategory(category2);
+			
+			assertFalse(film.getCategories().contains(category)
+					&& film.getCategories().contains(category2));
+		}
+		@Test
+		void mergeTest() {
+			var film = new Film("Hola mundo", new Language(2), (byte)1, new BigDecimal(10.0), 1, new BigDecimal(10.0));
+			var actor1 = new Actor(0, "Marc", "Roles");
+			var actor2 = new Actor(1, "Jose", "Antonio");
+			
+			var category = new Category(2);
+			var category2 = new Category(3);
+			film.addActor(actor1);
+			film.addActor(actor2);
+			film.addCategory(category);
+			film.addCategory(category2);
+			
+			var film2 = new Film("Adios mundo", new Language(2), (byte)1, new BigDecimal(10.0), 1, new BigDecimal(10.0));
+			var actor3 = new Actor(0, "Marc", "Roles");
+			
+			var category3 = new Category(4);
+			film.addActor(actor3);
+			film.addCategory(category3);
+			
+			film.merge(film2);
+			
+			assertAll("Multiple tests", 
+					() -> assertTrue(film.getActors().contains(actor1)),
+					() -> assertTrue(film.getActors().contains(actor3)),
+					() -> assertTrue(film.getCategories().contains(category3)));
 		}
 	}
 	
