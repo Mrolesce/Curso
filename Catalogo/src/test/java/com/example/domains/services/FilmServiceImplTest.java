1053838da37a0e0954bea6c41bd686f2620ccec1
mrolesce <mrolesce@LES-5CG0149KN3.corp.capgemini.com>
2023-04-03 12:56:01 +0200

    testAdd film

--- Catalogo/src/test/java/com/example/domains/services/FilmServiceImplTest.java
+++ Catalogo/src/test/java/com/example/domains/services/FilmServiceImplTest.java
@@ -1,23 +1,46 @@
 package com.example.domains.services;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+import java.math.BigDecimal;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
+
+import com.example.domains.contracts.repositories.FilmRepository;
+import com.example.domains.entities.Film;
+import com.example.domains.entities.Language;
+import com.example.exceptions.DuplicateKeyException;
+import com.example.exceptions.InvalidDataException;
 
 @DataJpaTest
 @ComponentScan(basePackages = "com.example")
 class FilmServiceImplTest {
 
+	@MockBean
+	FilmRepository dao;
+	
+	@Autowired
+	FilmServiceImpl srv;
+	
 	@BeforeEach
 	void setUp() throws Exception {
 	}
 
 	@Test
-	void testAdd() {
-		fail("Not yet implemented");
+	void testAdd() throws DuplicateKeyException, InvalidDataException {
+		var film = new Film("Hola mundo", new Language(2), (byte)1, new BigDecimal(10.0), 1, new BigDecimal(10.0));
+		
+		when(dao.save(film)).thenReturn(film);
+		
+		var rslt = srv.add(film);
+		
+		assertEquals(film, rslt);
 	}
 
 	@Test
