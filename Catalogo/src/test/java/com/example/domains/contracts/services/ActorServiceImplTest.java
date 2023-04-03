58782e65e953600c67071f13257262d6e1b230b1
mrolesce <mrolesce@LES-5CG0149KN3.corp.capgemini.com>
2023-04-03 10:43:20 +0200

    add, modify and delete actorserviceimpl tests

--- Catalogo/src/test/java/com/example/domains/contracts/services/ActorServiceImplTest.java
+++ Catalogo/src/test/java/com/example/domains/contracts/services/ActorServiceImplTest.java
@@ -1,6 +1,8 @@
 package com.example.domains.contracts.services;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
@@ -9,14 +11,18 @@ import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.dao.DuplicateKeyException;
 
 import com.example.domains.contracts.repositories.ActorRepository;
 import com.example.domains.entities.Actor;
 import com.example.domains.services.ActorServiceImpl;
+import com.example.exceptions.InvalidDataException;
+import com.example.exceptions.NotFoundException;
 
 @DataJpaTest
 @ComponentScan(basePackages = "com.example")
@@ -34,7 +40,7 @@ class ActorServiceImplTest {
 	}
 
 	@Test
-	void test() {
+	void getAllTest() {
 		List<Actor> lista = new ArrayList<>(
 				Arrays.asList(
 						new Actor(1, "Pepito", "GRILLO"),
@@ -43,7 +49,42 @@ class ActorServiceImplTest {
 		
 		when(dao.findAll()).thenReturn(lista);
 		
-		assertEquals(lista, dao.findAll());
+		var rslt = srv.getAll();
+		
+		assertEquals(lista, rslt);
+	}
+	@Test
+	void addTest() throws DuplicateKeyException, InvalidDataException {
+		
+		var actor = new Actor(1, "Pepito", "GRILLO");
+		
+		when(dao.save(actor)).thenReturn(actor);
+		
+		var rslt = srv.add(actor);
+		
+		assertEquals(actor, rslt);
+	}
+	@Test
+	void modifyTest() throws InvalidDataException, NotFoundException {
+		
+		var actor = new Actor(1, "Pepito", "GRILLO");
+		
+		when(dao.existsById(1)).thenReturn(true);
+		when(dao.save(actor)).thenReturn(actor);
+		
+		var rslt = srv.modify(actor);
+		verify(dao).existsById(1);
+		assertEquals(actor, rslt);
+	}
+	@Test
+	void deleteTest() throws InvalidDataException, NotFoundException {
+		
+		var actor = new Actor(0, "Pepito", "GRILLO");
+		
+		srv.deleteById(0);
+		
+		verify(dao).deleteById(0);
+		
 	}
 
 }
