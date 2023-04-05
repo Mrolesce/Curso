c7066ca5c571ea51491d71ce9c19146435ab2db1
mrolesce <mrolesce@LES-5CG0149KN3.corp.capgemini.com>
2023-04-05 14:13:07 +0200

    ActorResource creado

---
+++ demo/src/main/java/com/example/application/resources/ActorResource.java
@@ -0,0 +1,12 @@
+package com.example.application.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = {"/api/actores/v1", "/api/actors"})
+public class ActorResource {
+	
+	
+
+}
