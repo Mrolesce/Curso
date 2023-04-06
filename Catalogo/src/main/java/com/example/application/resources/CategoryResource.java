a05d85de7e8ad9c0d1894e45f1fa5018ae7ee2bc
mrolesce <mrolesce@LES-5CG0149KN3.corp.capgemini.com>
2023-04-06 14:38:21 +0200

    delete añadido

--- Catalogo/src/main/java/com/example/application/resources/CategoryResource.java
+++ Catalogo/src/main/java/com/example/application/resources/CategoryResource.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -77,6 +78,12 @@ public class CategoryResource {
 		if(item.isEmpty())
 			throw new NotFoundException();
 		return item.get().getFilmCategories().stream().map(o -> new ElementoDTO<>(o.getFilm().getFilmId(), o.getFilm().getTitle())).toList();
+	}
+	
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void delete(@PathVariable int id) {
+		srv.deleteById(id);
 	}
 
 }
