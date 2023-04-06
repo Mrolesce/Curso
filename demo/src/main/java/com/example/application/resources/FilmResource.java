eb27383ed5d3730703135fa92b37693961774261
mrolesce <mrolesce@LES-5CG0149KN3.corp.capgemini.com>
2023-04-06 10:17:18 +0200

    elementoDto y fixes

--- demo/src/main/java/com/example/application/resources/FilmResource.java
+++ demo/src/main/java/com/example/application/resources/FilmResource.java
@@ -1,10 +1,18 @@
 package com.example.application.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.domains.contracts.services.FilmService;
+import com.example.domains.entities.Film;
+import com.example.domains.entities.dtos.ActorShort;
+import com.example.domains.entities.dtos.FilmShortDTO;
 
 import jakarta.validation.Validator;
 
@@ -18,6 +26,10 @@ public class FilmResource {
 	@Autowired
 	Validator validator;
 	
-	
-
+	@GetMapping
+	public List<FilmShortDTO> getAll(@RequestParam(required = false) String sort) {
+		if(sort != null)
+			return (List<FilmShortDTO>)srv.getByProjection(Sort.by(sort), FilmShortDTO.class);
+		return srv.getByProjection(FilmShortDTO.class);
+	}
 }
