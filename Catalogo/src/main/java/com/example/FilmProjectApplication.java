60934ab5891b7246cfb30b676aded0eb69d33396
mrolesce <mrolesce@LES-5CG0149KN3.corp.capgemini.com>
2023-04-12 09:51:27 +0200

    OpenApi documentation

--- Catalogo/src/main/java/com/example/FilmProjectApplication.java
+++ Catalogo/src/main/java/com/example/FilmProjectApplication.java
@@ -3,10 +3,23 @@ package com.example;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import jakarta.transaction.Transactional;
-
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Microservicio: Catalogo de peliculas",
+                version = "1.0",
+                description = "Ejemplo de Microservicio utilizando la base de datos **Sakila**.",
+                license = @License(name = "Apache 2.0", url = "https://www.apache.org/licenses/LICENSE-2.0.html"),
+                contact = @Contact(name = "Marc Roles", url = "https://github.com/Mrolesce", email = "support@example.com")
+        ),
+        externalDocs = @ExternalDocumentation(description = "Documentación del proyecto", url = "https://github.com/Mrolesce/Curso")
+)
 @SpringBootApplication
 public class FilmProjectApplication implements CommandLineRunner {
 
