0289d88864332724192822ae092fa79cc4ecb078
Marc Roles <marc.roles-centellas@capgemini.com>
2023-04-14 11:59:27 +0200

    js examples exports and imports

---
+++ fundamentos/modulo2.js
@@ -0,0 +1,7 @@
+let nombre = 'Modulo 2';
+function multiplica (a,b) { return a * b;}
+function resta(a, b) { return a - b }
+function calc(a, b) { return a + b }
+function display () { console.log(`Soy ${nombre}`);}
+
+export{ nombre, calc, multiplica, display }
