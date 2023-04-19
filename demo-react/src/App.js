adb9ff23055729989bb0433881d39a92e97681f7
Marc Roles <marc.roles-centellas@capgemini.com>
2023-04-19 09:19:55 +0200

    nav without style

--- demo-react/src/App.js
+++ demo-react/src/App.js
@@ -10,9 +10,58 @@ export default class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-        cont: 0
+        cont: 0,
+        main : 0
     }
+    this.menu = [
+      { text: 'inicio', url: '/', componente: <Home/>},
+      { text: 'demos', url: '/demos', componente: <DemoJSX/>},
+      { text: 'contador', url: '/contador', componente: <Contador init={10} delta={2} onChange={num => this.setState({cont: num})}/>},
+      { text: 'ejemplos', url: '/', componente: <Ejemplos/>}
+    ]
 }
+  render() {
+    return (
+      <>
+        <Cabecera menu={this.menu} onSelectMenu={indice => this.setState({main: indice})}/>
+        <main className="container-fluid">
+          {this.menu[this.state.main].componente}
+        </main>
+        <Pie />
+      </>
+    );
+  }
+}
+
+function Cabecera(props){
+  return (
+    <header>
+      <Menu {...props}/>
+    </header>
+  );
+}
+
+function Menu({menu, onSelectMenu}){
+  return (
+    <nav>
+      {menu.map((item, index) => <button key={index} type="button" onClick={() => onSelectMenu && onSelectMenu(index)}>{item.text}</button> )}
+    </nav>
+  );
+}
+
+function Pie(){
+  return null;
+}
+
+export class Ejemplos extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+        cont: 0,
+        main : 0
+    }
+    
+  }
   render() {
     return (
       <>
